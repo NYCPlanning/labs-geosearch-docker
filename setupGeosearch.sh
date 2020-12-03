04aad5e27a89b382c22d8753c3eb9b2d91d878a0
SPTKL <caobaiyue@gmail.com>
2020-12-03 13:05:54 -0500

    Update setupGeosearch.sh

--- setupGeosearch.sh
+++ setupGeosearch.sh
@@ -6,13 +6,27 @@ sudo su - pelias
 # create elasticsearch data mount dir with correct permissions
 cd geosearch
 mkdir -p data/elasticsearch
+mkdir -p data/nycpad && (
+  cd data/nycpad
+  curl -O https://planninglabs.nyc3.digitaloceanspaces.com/geosearch-data/labs-geosearch-pad-normalized-sample-md.zip
+  unzip labs-geosearch-pad-normalized-sample-md.zip
+  mv labs-geosearch-pad-normalized-sample-md.csv labs-geosearch-pad-normalized.csv
+  rm *.zip
+  ls
+)
+sudo chown 1000:1000 -R data
+sudo chown 1000:1000 -R data/elasticsearch
+sudo chown 1000:1000 -R data/nycpad
+ls -n data
 
-# Set up and run geocoder
-echo "Pulling docker resources..."
-./pelias compose pull
+export DATA_DIR=$(pwd)/data
 
 echo "Bringing up elasticsearch..."
 ./pelias compose up elasticsearch
+./pelias wait
+./pelias elastic create
+./pelias elastic indices
+./pelias import nycpad
 
 echo "Bringing up libpostal, and api..."
 ./pelias compose up api libpostal
