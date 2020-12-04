9566df200be16f71a744cd570bd28f473694994e
sptkl <caobaiyue@gmail.com>
2020-12-04 10:08:20 -0500

    Adding support for terraform deployment - provider.tf -> describes digitalocean as a provider - geosearch.tf -> describes server configuration, steps to deploy geosearch - pelias.json -> had to change indexName so that the process can finish without bug

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+
+variable "do_token" {}
+variable "pvt_key" {}
+variable "pub_key" {}
+variable "pwd" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "terraform" {
+  name = "baiyue"
+}
