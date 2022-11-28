9d9a1d32e3b0b6f0f3058e666f80dbcc10e42d38
Tyler Matteo <tylermatteo434@gmail.com>
2022-11-28 15:00:11 -0500

    Comment out geosearch ssh key config

--- provider.tf
+++ provider.tf
@@ -17,9 +17,9 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-data "digitalocean_ssh_key" "terraform" {
-  name = "terraform"
-}
+# data "digitalocean_ssh_key" "terraform" {
+#   name = "terraform"
+# }
 
 data "digitalocean_ssh_key" "geosearch" {
   name = "geosearch"
