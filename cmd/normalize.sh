38715c5e4c9c6f4797e9cb89499feae693e744d5
Tyler Matteo <tylermatteo434@gmail.com>
2022-12-06 10:35:13 -0500

    Replace contents of cmd and lib folder with those in current version of pelias/docker repo

--- cmd/normalize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e;
-
-function normalize_nycpad() { compose_run 'nycpad_normalizer' $@; }
-register 'normalize' 'nycpad' '(re)download nycpad data, normalize, and save; version can optionally be specified' normalize_nycpad
-
